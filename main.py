f4f92f6e615080614b30f45b054cbfdf7d64c401
manfred.madelaine <manfred.madelaine@outlook.fr>
2020-11-08 01:10:36 +0100

    :sparkles: Clean the "board to text" converter

--- main.py
+++ main.py
@@ -1,42 +1,16 @@
-
-
 class Sudoku:
     grid = []
     size = 0
     hilighted_values = []
-    
 
     def __str__(self):
-        txt = ""
-        main_sep = 3
-
-        main_sep_v, main_sep_h, sub_sep_v, sub_sep_h = {
-            "heavy": ['||', '=',   ':', '-'],
-            "light": ['|' , '-',    ' ', ' '],
-        }["light"]
-
-        hilight_char = ["*", "^"][0]
-
-        for i, row in enumerate(self.grid):
-            txt += "\n"
-            line = ''
-            sub_sep = ''
-            for j, r in enumerate(row):
-                h = hilight_char if r in self.hilighted_values else " "
-                c = f" {r}{h}" 
-
-                # Vertical separators
-                use_main_sep_v = j>0 and (j+1)%main_sep == 0
-                line += c + [sub_sep_v, main_sep_v][use_main_sep_v] if j < self.size-1 else c
-
-                # Horizontal separators
-                sub_sep += sub_sep_h*len(c) + [sub_sep_v, main_sep_v][use_main_sep_v] if j < self.size-1 else sub_sep_h*len(c)
-                use_main_sep_h = i>0 and (i+1)%main_sep == 0
-                sep = main_sep_h * len(line) if use_main_sep_h else sub_sep
-
-            txt += "\n".join([line, sep]) if i < self.size-1 else line
-
+        txt = board_to_text(self, self.grid, self.cell_to_text)
         return "The great Sudoku !" + txt
+
+    def cell_to_text(self, r):
+        h = "*" if r in self.hilighted_values else " "
+        c = f" {r}{h}"
+        return c
 
     def set_grid(self, grid):
         if isinstance(grid, str):
@@ -52,8 +26,46 @@ def txt_to_list(txt):
     return txt.replace(" ", "").split("\n")[1:-1]
 
 
+def board_to_text(object, grid, cell_to_text):
+    txt = ""
+    main_sep = 3
+
+    main_sep_v, main_sep_h, sub_sep_v, sub_sep_h = {
+        "heavy": ["||", "=", ":", "-"],
+        "light": ["|", "-", " ", " "],
+    }["light"]
+
+    for i, row in enumerate(grid):
+        txt += "\n"
+        line = ""
+        sub_sep = ""
+        for j, r in enumerate(row):
+            c = object.cell_to_text(r)
+
+            # Vertical separators
+            use_main_sep_v = j > 0 and (j + 1) % main_sep == 0
+            line += (
+                c + [sub_sep_v, main_sep_v][use_main_sep_v]
+                if j < object.size - 1
+                else c
+            )
+
+            # Horizontal separators
+            sub_sep += (
+                sub_sep_h * len(c) + [sub_sep_v, main_sep_v][use_main_sep_v]
+                if j < object.size - 1
+                else sub_sep_h * len(c)
+            )
+            use_main_sep_h = i > 0 and (i + 1) % main_sep == 0
+            sep = main_sep_h * len(line) if use_main_sep_h else sub_sep
+
+        txt += "\n".join([line, sep]) if i < object.size - 1 else line
+
+    return txt
+
+
 def main():
-    sudoku = """
+    raw_sudoku = """
         004903008
         003050002
         978200000
