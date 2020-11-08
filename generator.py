65645ffdeed04041680e743e508cdd21fae76ea9
Ruben Madelaine <madelaine.ruben@hotmail.com>
2020-11-08 02:25:09 +0100

    :white_check_mark: Add Validator

--- generator.py
+++ generator.py
@@ -1,52 +1,60 @@
 import numpy as np
 from random import shuffle
+from validator import valid_solution
 
 
 MIX_LOOP = 4
 sub = 3
-size = sub**2
+size = sub ** 2
 
 
-def generate_grid(sub, size):
+def generate_board(sub, size):
     def add_offset(list, offset):
         s = len(list)
-        return list[s-offset:] + list[:s-offset]
+        return list[s - offset :] + list[: s - offset]
 
-    grid = []    
-    vals = list(range(1, size+1))
+    board = []
+    vals = list(range(1, size + 1))
 
     for i in range(size):
-        grid += [vals]
+        board += [vals]
 
         vals = add_offset(vals, sub)
-        if i>0 and (i+1)%sub == 0:
+        if i > 0 and (i + 1) % sub == 0:
             vals = add_offset(vals, 1)
 
-    return grid
+    return board
 
 
-def randomizer(grid):
-	for i in range(2):
-		random_grid = []
-		for i in range(0, len(grid), 3):
-			a = grid[i:i+3]
-			shuffle(a)
-			random_grid += a
-	return random_grid
+def randomizer(board):
+    for i in range(2):
+        random_board = []
+        for i in range(0, len(board), 3):
+            a = board[i : i + 3]
+            shuffle(a)
+            random_board += a
+    return random_board
 
 
-def mix_loop(grid,num):
-	for i in range(num):
-		grid = randomizer(grid)
-		grid = np.transpose(grid).tolist()
+def mix_loop(board, num):
+    for i in range(num):
+        board = randomizer(board)
+        board = np.transpose(board).tolist()
 
-	return grid
+    return board
+
+
+def validation_test(board):
+    test = valid_solution(board)
+    return test
+
 
 def main(num):
-	filled_grid = generate_grid(sub, size)
-	random_grid = mix_loop(filled_grid,num)
+    filled_board = generate_board(sub, size)
+    random_board = mix_loop(filled_board, num)
 
-	print(*random_grid, sep = "\n")
+    print(*random_board, sep="\n")
+    print(validation_test(random_board))
 
 
 main(MIX_LOOP)
