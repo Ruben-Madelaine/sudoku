0321fe02926ec069c65df8002a898d5789879bf2
Ruben Madelaine <madelaine.ruben@hotmail.com>
2020-11-10 19:14:05 +0100

    :construction: Stable
    * Lose the sudoku ID

--- cleaner.py
+++ cleaner.py
@@ -6,15 +6,15 @@ import numpy as np
 
 LEVEL = 2
 BOARD = [
-        [4, 5, 6, 8, 9, 7, 1, 3, 2],
-        [1, 2, 3, 5, 6, 4, 7, 9, 8],
-        [7, 8, 9, 2, 3, 1, 4, 6, 5],
-        [6, 7, 8, 1, 2, 9, 3, 5, 4],
-        [9, 1, 2, 4, 5, 3, 6, 8, 7],
-        [3, 4, 5, 7, 8, 6, 9, 2, 1],
-        [5, 6, 7, 9, 1, 8, 2, 4, 3],
-        [2, 3, 4, 6, 7, 5, 8, 1, 9],
-        [8, 9, 1, 3, 4, 2, 5, 7, 6],
+        [1, 2, 3, 5, 4, 6, 8, 7, 9],
+        [7, 8, 9, 2, 1, 3, 5, 4, 6],
+        [4, 5, 6, 8, 7, 9, 2, 1, 3],
+        [9, 1, 2, 4, 3, 5, 7, 6, 8],
+        [6, 7, 8, 1, 9, 2, 4, 3, 5],
+        [3, 4, 5, 7, 6, 8, 1, 9, 2],
+        [2, 3, 4, 6, 5, 7, 9, 8, 1],
+        [5, 6, 7, 9, 8, 1, 3, 2, 4],
+        [8, 9, 1, 3, 2, 4, 6, 5, 7]
         ]
 
 
@@ -31,29 +31,40 @@ def clean(level, board):
         count = 0
 
         for row in range(len(board)):
-            for elem_selected in range(len(board)):
-                if board[row][elem_selected] == elem_to_replace+1 and count < limit:
-                    count += 1
-                    board[row][elem_selected] = 0
+            for column in range(len(board)):
+                if board[row][column] == elem_to_replace+1:# and count < limit:
+                    print(elem_to_replace+1)
+                    condition = cleanable(board, row, column)
+                    if condition:
+                        count += 1
+                        board[row][column] = 0
     return board
 
-
-def cleanable(board, row, elem_selected):
-    board = np.array(board)
-    local_square_angle = 
-    local_square = 
-
-
-
-
-
-    if board[i:,] == board[:i,] == selection ==
-    print(board)
 
 def random_value(level):
     selection = dificulty(level)
     level = random.randrange(selection[0], selection[1])
     return level
+
+
+def cleanable(board, column, row):
+    sqrt = int(len(board)**(1/2))
+    angle1 = column//sqrt*sqrt
+    angle2 = column//sqrt*sqrt+sqrt
+    angle3 = row//sqrt*sqrt
+    angle4 = row//sqrt*sqrt+sqrt
+    matrix = np.array(board)[angle1 : angle2, angle3 : angle4].tolist()
+    set_matrix = set(sum(matrix, []))
+    condition = len(set_matrix)
+
+
+
+    print("row:", row, "column:", column, "val:", board[row][column])
+    print("angle1", angle1, "angle2", angle2, "angle3", angle3, "angle4", angle4)
+    print(matrix)
+    print("\n")
+    # import pdb; pdb.set_trace()
+    return condition > 2
 
 
 def show(board):
@@ -64,11 +75,9 @@ def show(board):
 
 
 def main():
+    # board = clean(LEVEL, randomize(board, int(len(board)**(1/2))))
     board = clean(LEVEL,BOARD)
-    print(*board, sep='\n')
-    # board = randomize(board, int(len(board)**(1/2)))
     show(board)
 
-# main()
 
-cleanable(BOARD)
+main()
